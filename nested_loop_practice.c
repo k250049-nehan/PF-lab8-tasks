d02f70b097dec86bfeace01ee100e96d5f1db352
Muhammad Nehan Malik <k250049@nu.edu.pk>
2025-10-23 09:31:52 +0500

    Update and rename nestedloop.c to nested_loop_practice.c

--- nested_loop_practice.c
+++ nested_loop_practice.c
@@ -32,16 +32,6 @@ int main()
         }
         printf("\n");
     }
-
-    printf("\n===Multiplication of tables(5x5)===\n\n");
-    for (i = 1; i <= 5; i++)
-    {
-        printf("%4d. ", i);
-        for (j = 1; j <= 5; j++)
-        {
-            printf("%4d  ", i * j);
-        }
-        printf("\n");
-    }
     return 0;
+
 }
