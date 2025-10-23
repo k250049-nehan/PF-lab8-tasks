bbb8ae27a1f2d2e555eaa43019146bb6c9d0541b
Muhammad Nehan Malik <k250049@nu.edu.pk>
2025-10-23 09:31:09 +0500

    Add files via upload

---
+++ nestedloop5x5.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+  int i,j;
+   printf("\n===Multiplication of tables(5x5)===\n\n");
+    for (i = 1; i <= 5; i++)
+    {
+        printf("%4d. ", i);
+        for (j = 1; j <= 5; j++)
+        {
+            printf("%4d  ", i * j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
